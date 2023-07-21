2a267d16ce411a027410807eaf6f03d0b9f99b4b
Luis Arturo Pacheco <luigipach@gmail.com>
2023-07-21 16:16:47 -0400

    updated

--- python/example.py
+++ python/example.py
@@ -1,5 +1,8 @@
 from kukavarproxy import *
+import time
 robot = KUKA('172.31.1.147')
-
-robot.write("$OV_PRO",33)
-print (robot.read("$OV_PRO"))
+pos_cur = robot.read("$POS_ACT")
+print(pos_cur)
+robot.write("C3BI_JOG_FRAME","{FRAME: X 1.0, Y 0.0, Z 0.0, A 0.0, B 0.0, C 0.0}")
+robot.write("C3BI_JOG_FRAME","{FRAME: X 0.0, Y 0.0, Z 0.0, A 0.0, B 0.0, C 0.0}")
+print (robot.read("C3BI_JOG_FRAME"))
