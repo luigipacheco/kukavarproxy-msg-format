7d1f311dc95b7b0829b2bdd7c1868c8db7aa9ead
Luis Arturo Pacheco <luigipach@gmail.com>
2024-03-01 17:02:53 -0500

    test

--- python/example.py
+++ python/example.py
@@ -3,6 +3,6 @@ import time
 robot = KUKA('172.31.1.147')
 pos_cur = robot.read("$POS_ACT")
 print(pos_cur)
-robot.write("C3BI_JOG_FRAME","{FRAME: X 1.0, Y 0.0, Z 0.0, A 0.0, B 0.0, C 0.0}")
+""" robot.write("C3BI_JOG_FRAME","{FRAME: X 1.0, Y 0.0, Z 0.0, A 0.0, B 0.0, C 0.0}")
 robot.write("C3BI_JOG_FRAME","{FRAME: X 0.0, Y 0.0, Z 0.0, A 0.0, B 0.0, C 0.0}")
-print (robot.read("C3BI_JOG_FRAME"))
+print (robot.read("C3BI_JOG_FRAME")) """
